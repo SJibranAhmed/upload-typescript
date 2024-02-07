4c0141cbfb5511dfa0077ad7d16783259790f60b
SJibran Ahmed <Sheikhjibran889970@gmail.com>
2024-02-07 21:15:37 +0500

    typescipt

---
+++ third.js
@@ -0,0 +1,6 @@
+var naam = 'Jibran';
+console.log(naam);
+var kaam = false;
+console.log(kaam);
+var ihtemam = 'biryani';
+console.log(ihtemam);
---
+++ third.ts
@@ -0,0 +1,11 @@
+let naam:string = 'Jibran'
+
+console.log(naam)
+
+let kaam:boolean = false
+
+console.log(kaam)
+
+let ihtemam:string = 'biryani'
+
+console.log(ihtemam)
